687c5d2a7e64495c4a34c151a653575486d76a0b
kgnot <hricaurtem@proton.me>
2026-05-06 09:47:37 -0500

    error de importación

--- src/algoritmos/edmons-karp/edmons-karp.ts
+++ src/algoritmos/edmons-karp/edmons-karp.ts
@@ -1,7 +1,7 @@
 import type { Graph } from "../../estructuras/graph/graph.js";
 import { GraphIndexer } from "../../estructuras/graph/graph_indexer.js";
 import { toAdjacencyMatrix } from "../../estructuras/proyeccion/adjacency_matrix.js";
-import { BFS } from "../bfs/bfs.js";
+import { BFS } from "../dfs/bfs.js";
 
 export class EdmonsKarp<T> {
 	private residualMatrix: number[][];
